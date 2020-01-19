390086c4e23dc8d8d86bb5cb0f65c729548cbc2a
tsk-nkgw <tasktas9@gmail.com>
2020-01-19 16:04:23 +0900

    fix

--- src/main/kotlin/t45k/ghcbonk/MyLambda.kt
+++ src/main/kotlin/t45k/ghcbonk/MyLambda.kt
@@ -5,17 +5,19 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 import t45k.ghcbonk.github.ContributionData
 import t45k.ghcbonk.github.GitHubUser
 import t45k.ghcbonk.twitter.Tweeter
+import java.io.InputStream
+import java.net.URL
 import java.util.Properties
+import java.util.ResourceBundle
 
 class MyLambda : RequestHandler<Unit, String> {
     override fun handleRequest(input: Unit, context: Context?): String {
         return try {
-            val properties = Properties()
-            properties.load(this.javaClass.getResourceAsStream("/resources/resource.properties"))
-            val userName: String = properties.getProperty("userName")
+            val propertyBundle: ResourceBundle = ResourceBundle.getBundle("info")
+            val userName: String = propertyBundle.getString("userName")
             val user = GitHubUser(userName)
             val contributionData: ContributionData = user.fetchContributionData()
-            val tweeter = Tweeter(properties)
+            val tweeter = Tweeter(propertyBundle)
             tweeter.tweet(contributionData)
 
             "success"
