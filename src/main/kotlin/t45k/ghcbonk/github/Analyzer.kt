e1013ce6b21c1b4fa23d66b07274fd2a4a488423
T45K <tasktas9@gmail.com>
2021-02-10 22:55:05 +0900

    fix

--- src/main/kotlin/t45k/ghcbonk/github/Analyzer.kt
+++ src/main/kotlin/t45k/ghcbonk/github/Analyzer.kt
@@ -5,7 +5,7 @@ fun analyzeRawData(rawData: List<String>, userName: String): ContributionData {
 
     for (oneDayActivity: String in rawData.reversed()) {
         val elements: List<String> = oneDayActivity.split(Regex("\\s+"))
-        val numOfContributionOfTheDay: String = getStringInDoubleQuote(elements[8])
+        val numOfContributionOfTheDay: String = getStringInDoubleQuote(elements[9])
 
         if (numOfContributionOfTheDay == "0") {
             break
@@ -15,8 +15,8 @@ fun analyzeRawData(rawData: List<String>, userName: String): ContributionData {
     }
 
     val lastActivity: List<String> = rawData.last().split(Regex("\\s+"))
-    val namOfContribution: String = getStringInDoubleQuote(lastActivity[8])
-    val date: String = getStringInDoubleQuote(lastActivity[9])
+    val namOfContribution: String = getStringInDoubleQuote(lastActivity[9])
+    val date: String = getStringInDoubleQuote(lastActivity[10])
 
     return ContributionData(userName, date, namOfContribution, counter)
 }
