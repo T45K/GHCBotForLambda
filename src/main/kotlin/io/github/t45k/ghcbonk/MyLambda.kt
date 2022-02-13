2bf684d7854846dbee82d9e8aa08401e0473a0a1
T45K <tasktas9@gmail.com>
2022-02-13 23:21:12 +0900

    refactor

--- src/main/kotlin/io/github/t45k/ghcbonk/MyLambda.kt
+++ src/main/kotlin/io/github/t45k/ghcbonk/MyLambda.kt
@@ -8,11 +8,12 @@ import io.github.t45k.ghcbonk.github.analyzeDocument
 import io.github.t45k.ghcbonk.twitter.TwitterClient
 import io.github.t45k.ghcbonk.twitter.tweetModel.SimpleTweetModel
 import io.github.t45k.ghcbonk.twitter.tweetModel.WordleLikeTweetModel
+import io.github.t45k.ghcbonk.util.ResponseMixin
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.util.ResourceBundle
 
-class MyLambda : RequestHandler<Unit, Unit> {
+class MyLambda : RequestHandler<Unit, Unit>, ResponseMixin {
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)
     }
@@ -25,8 +26,6 @@ class MyLambda : RequestHandler<Unit, Unit> {
             .let(::analyzeDocument)
 
         val today = LocalDate.now()
-        val simpleTweet = SimpleTweetModel(githubUserName)
-        val wordleLikeTweet = WordleLikeTweetModel()
 
         val twitterClient = TwitterClient(
             property.getString("apiKey"),
@@ -34,16 +33,17 @@ class MyLambda : RequestHandler<Unit, Unit> {
             property.getString("token"),
             property.getString("tokenSecret")
         )
-        val response1 = twitterClient.tweet(simpleTweet.getContent(today, contributionCounts))
-        if (response1.code != 200) {
-            logger.warn("Failure for Simple Tweet")
-            logger.warn(response1.body?.string())
-        }
 
-        val response2 = twitterClient.tweet(wordleLikeTweet.getContent(today, contributionCounts))
-        if (response2.code != 200) {
-            logger.warn("Failure for Wordle-like Tweet")
-            logger.warn(response2.body?.string())
+        val tweetModels = listOf(
+            SimpleTweetModel(githubUserName),
+            WordleLikeTweetModel()
+        )
+        for (tweetModel in tweetModels) {
+            val response = twitterClient.tweet(tweetModel.getContent(today, contributionCounts))
+            if (response.isFailure()) {
+                logger.warn("Failure for ${tweetModel.javaClass.simpleName}")
+                logger.warn(response.body?.string())
+            }
         }
     }
 }
