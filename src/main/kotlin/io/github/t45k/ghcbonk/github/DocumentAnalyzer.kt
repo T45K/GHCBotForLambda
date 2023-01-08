ec3b35f74bb0081d18bfaf2007e4f40605935148
T45K <tasktas9@gmail.com>
2023-01-09 00:45:09 +0900

    fix impl

--- src/main/kotlin/io/github/t45k/ghcbonk/github/DocumentAnalyzer.kt
+++ src/main/kotlin/io/github/t45k/ghcbonk/github/DocumentAnalyzer.kt
@@ -3,7 +3,16 @@ package io.github.t45k.ghcbonk.github
 import org.jsoup.nodes.Document
 import java.time.LocalDate
 
+private const val CONTRIBUTION_CALENDAR_CSS_NAME = "ContributionCalendar-day"
+private const val DATE_ATTR_NAME = "data-date"
+
 fun analyzeDocument(document: Document): List<ContributionCount> =
-    document.select("rect[data-count]")
-        .sortedBy { it.attr("data-date") }
-        .map { LocalDate.parse(it.attr("data-date")) to it.attr("data-count").toInt() }
+    document.select(".$CONTRIBUTION_CALENDAR_CSS_NAME")
+        .filter { it.hasAttr(DATE_ATTR_NAME) }
+        .sortedBy { it.attr(DATE_ATTR_NAME) }
+        .map { LocalDate.parse(it.attr(DATE_ATTR_NAME)) to parseCalendarMessage(it.text()) }
+
+/**
+ * @param message like "1 contribution on January 9, 2023" or "No contribution on January 9, 2023"
+ */
+private fun parseCalendarMessage(message: String): Int = message.split(" ").first().toIntOrNull() ?: 0
