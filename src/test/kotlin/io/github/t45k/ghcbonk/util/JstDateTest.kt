ac8c0a28422bcc680fa64032bba7d176345bcca0
T45K <tasktas9@gmail.com>
2024-05-18 22:07:01 +0900

    fix

--- src/test/kotlin/io/github/t45k/ghcbonk/util/JstDateTest.kt
+++ src/test/kotlin/io/github/t45k/ghcbonk/util/JstDateTest.kt
@@ -2,6 +2,8 @@ package io.github.t45k.ghcbonk.util
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import java.time.LocalDate
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class JstDateTest {
@@ -9,5 +11,11 @@ class JstDateTest {
     fun from() {
         val dateTime = ZonedDateTime.parse("2024-01-02T15:06:07Z")
         assertEquals(ZonedDateTime.parse("2024-01-03T00:00:00+09:00").toInstant(), JstDate.from(dateTime).toInstant())
+    }
+
+    @Test
+    fun now() {
+        val dateTime = LocalDate.now().atStartOfDay().atZone(ZoneId.of("Asia/Tokyo"))
+        assertEquals(JstDate.now().toInstant(), dateTime.toInstant())
     }
 }
